80e490819a4136594bfb3c93b49456b4d4c3eed0
PaulElcampeon <pauloladele@live.co.uk>
2020-05-12 23:01:03 +0100

    First commit with example scene created.

---
+++ Assets/Scripts/Ball.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Ball : MonoBehaviour
+{
+    public static Ball instance;
+
+    private void Awake()
+    {
+        if (instance == null)
+        {
+            instance = this;
+        } else
+        {
+            Destroy(gameObject);
+        }
+
+        DontDestroyOnLoad(this.gameObject);
+    }
+}
---
+++ Assets/Scripts/Magnet.cs
@@ -0,0 +1,85 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Magnet : MonoBehaviour
+{
+
+    [Header("Magnet Button")]
+    [SerializeField]
+    private Button magnetSwitch;
+
+    [Header("Magnet Strength")]
+    [SerializeField]
+    public float speed;
+
+    private bool isActive;
+    private GameObject[] magnets;
+
+    void Start()
+    {
+        magnets = GameObject.FindGameObjectsWithTag("Magnet");
+        TurnButtonRed();
+    }
+
+    void FixedUpdate()
+    {
+        if (isActive) Ball.instance.gameObject.GetComponent<Rigidbody2D>().MovePosition(Vector3.MoveTowards(Ball.instance.gameObject.transform.position, transform.position, this.speed * Time.deltaTime));
+    }
+
+    public void Hit()
+    {
+
+        if (this.isActive)
+        {
+            this.Deactivate();
+        }
+        else
+        {
+            this.Activate();
+        }
+    }
+
+    private void Activate()
+    {
+        DeactivateAllOtherMagnets();
+        TurnButtonGreen();
+        this.isActive = true;
+    }
+
+    private void TurnButtonGreen()
+    {
+        ColorBlock buttonColors = magnetSwitch.colors;
+        buttonColors.normalColor = new Color32(169, 255, 76, 255);
+        buttonColors.highlightedColor = new Color32(141, 241, 35, 255);
+        buttonColors.selectedColor = new Color32(169, 255, 76, 255);
+        magnetSwitch.colors = buttonColors;
+    }
+
+    private void TurnButtonRed()
+    {
+        ColorBlock buttonColors = magnetSwitch.colors;
+        buttonColors.normalColor = new Color32(255, 76, 76, 255);
+        buttonColors.highlightedColor = new Color32(248, 11, 11, 255);
+        buttonColors.selectedColor = new Color32(255, 76, 76, 255);
+        magnetSwitch.colors = buttonColors;
+    }
+
+    private void Deactivate()
+    {
+        TurnButtonRed();
+        this.isActive = false;
+    }
+
+    private void DeactivateAllOtherMagnets()
+    {
+        foreach(GameObject magnet in magnets)
+        {
+            if (magnet.GetInstanceID() != gameObject.GetInstanceID())
+            {
+                magnet.GetComponent<Magnet>().Deactivate();
+            }
+        }
+    }
+}
