026b8e3e369805e11e782ad38ba76d2f86497a8f
PaulElcampeon <pauloladele@live.co.uk>
2020-05-17 23:45:21 +0100

    Adding boundaries for different size screens.

--- Assets/Scripts/GameManager.cs
+++ Assets/Scripts/GameManager.cs
@@ -6,6 +6,21 @@ public class GameManager : MonoBehaviour
 {
     private GameObject[] magnets;
 
+    [SerializeField]
+    private GameObject nineteenByNine;
+
+    [SerializeField]
+    private GameObject sixteenByNine;
+
+    [SerializeField]
+    private GameObject eighteenByNine;
+
+    [SerializeField]
+    private GameObject fiveByThree;
+
+    [SerializeField]
+    private GameObject thirtySevenByEighteen;
+
     public static GameManager instance;
 
     private void Awake()
@@ -19,5 +34,43 @@ public class GameManager : MonoBehaviour
         }
 
         DontDestroyOnLoad(this.gameObject);
+    }
+
+    private void Start()
+    {
+        float aspectRatio = Camera.main.aspect;
+
+        if (aspectRatio == 16f / 9f)
+        {
+            sixteenByNine.SetActive(true);
+            Debug.Log("16by9");
+
+        }
+        else if (aspectRatio == 19f / 9f)
+        {
+            nineteenByNine.SetActive(true);
+            Debug.Log("19by9");
+
+        }
+        else if (aspectRatio == 18f / 9f)
+        {
+            eighteenByNine.SetActive(true);
+            Debug.Log("18by3");
+
+        }
+        else if (aspectRatio == 5f / 3f)
+        {
+            fiveByThree.SetActive(true);
+            Debug.Log("5by3");
+        }
+        else if (aspectRatio == 37f / 18f)
+        {
+            thirtySevenByEighteen.SetActive(true);
+            Debug.Log("37by18");
+        }
+        else
+        {
+            sixteenByNine.SetActive(true);
+        }
     }
 }
